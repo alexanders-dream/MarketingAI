161e5bd52ce1e7c85e793ed6e232aaf7a3143c67
alexanders-dream <alexanderoguso@gmail.com>
2026-01-12 11:08:45 +0300

    fix2_2026

--- market_analyzer.py
+++ market_analyzer.py
@@ -6,7 +6,7 @@ import asyncio
 from typing import Dict, Any, Optional, List
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.vectorstores import FAISS
 
